f06fb80bb23f9445a7793496307f87b4c532da49
George Tinsley <george8s@live.co.uk>
2023-08-31 16:44:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { evaluate } from "mathjs";
+import "./App.css";
 
 function App() {
+  document.addEventListener("keydown", (e) => {
+    let pressed = `${e.key}`;
+    switch (pressed) {
+      case "c":
+      case "C":
+        setSum("");
+        break;
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+      case "0":
+      case "+":
+      case "/":
+      case "*":
+      case "-":
+        setSum(sum + pressed);
+        break;
+      default:
+        return;
+    }
+  });
+
+  const [sum, setSum] = useState("");
+  const buttons = [
+    "ANS",
+    "^",
+    "e",
+    "√",
+    "(",
+    ")",
+    "7",
+    "8",
+    "9",
+    "C",
+    "4",
+    "5",
+    "6",
+    "*",
+    "1",
+    "2",
+    "3",
+    "/",
+    "0",
+    "+",
+    ".",
+    "-",
+    "=",
+  ];
+
+  const handleClick = (button) => {
+    switch (button) {
+      case "=":
+        setSum(evaluate(sum));
+        break;
+      case "C":
+      case "c":
+        setSum("");
+        break;
+      case "√":
+        setSum(sum + "√(") 
+        break;
+      default:
+        setSum(sum + button);
+    }
+  };
+
+  const signs = buttons.map((button, index) => {
+    return (
+      <button
+        className={`button-${index}`}
+        key={index}
+        label={button}
+        onClick={() => handleClick(button)}
+      >
+        {button}
+      </button>
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="outputBox">
+          <div className="outputText">
+            <h2>{sum}</h2>
+          </div>
+        </div>
+        <div className="buttonLayout">{signs}</div>
+      </div>
     </div>
   );
 }
